0071d35ff4d69fc83315359931c6393faa65d6ba
DoHuyHoang <dohuyhoangvn93@gmail.com>
2025-10-22 17:07:53 +0700

    CI: Add pre-compiled WASM package for GitHub Pages

---
+++ pkg/calsdt_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const analyze: (a: number, b: number, c: any) => any;
+export const quick_check_single_number: (a: number, b: number, c: any) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_start: () => void;
